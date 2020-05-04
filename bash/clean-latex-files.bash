a76273d70068bc49513fa0b1ce42b5ff1272e4f1
ibalampanis <ibalampanis@isc.tuc.gr>
2020-05-04 11:31:31 +0300

    Minor changes.
    Rerun some experiments to give more stable stats.
    (Experiment: Accuracy and number of rounds for various
    numbers of LSTM Cells.

--- bash/clean-latex-files.bash
+++ bash/clean-latex-files.bash
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 cd ../latex/proposal
-
 rm -f proposal.aux proposal.bbl proposal.bcf proposal.blg
 rm -f proposal.log proposal.out proposal.run.xml proposal.synctex.gz
+
+cd ../thesis
+rm -f thesis.aux thesis.bbl thesis.bcf thesis.blg thesis.lof thesis.toc
+rm -f thesis.log thesis.out thesis.run.xml thesis.synctex.gz config.aux
 
 cd ../../
 rm -rf out/

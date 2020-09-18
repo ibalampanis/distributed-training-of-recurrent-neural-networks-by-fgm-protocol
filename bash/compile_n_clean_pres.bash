b064aa0f4e3c98b2c5af200bc23756d092a1eb4d
ibalampanis <ibalampanis@isc.tuc.gr>
2020-09-19 00:33:15 +0300

    Presentation is building now...

--- bash/compile_n_clean_pres.bash
+++ bash/compile_n_clean_pres.bash
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+# shellcheck disable=SC2164
 cd /home/ibalampanis/CLionProjects/distributed-training-of-recurrent-neural-networks-by-fgm-protocol/latex/presentation/
 
 /bin/bash /home/ibalampanis/CLionProjects/distributed-training-of-recurrent-neural-networks-by-fgm-protocol/bash/cleanup_latex_pres.bash
-rm -f presentation.pdf
+rm -f main.pdf
 
-pdflatex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=/home/ibalampanis/CLionProjects/distributed-training-of-recurrent-neural-networks-by-fgm-protocol/latex/presentation presentation.tex
-pdflatex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=/home/ibalampanis/CLionProjects/distributed-training-of-recurrent-neural-networks-by-fgm-protocol/latex/presentation presentation.tex
+pdflatex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=/home/ibalampanis/CLionProjects/distributed-training-of-recurrent-neural-networks-by-fgm-protocol/latex/presentation main.tex
+pdflatex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=/home/ibalampanis/CLionProjects/distributed-training-of-recurrent-neural-networks-by-fgm-protocol/latex/presentation main.tex
 
 
 /bin/bash /home/ibalampanis/CLionProjects/distributed-training-of-recurrent-neural-networks-by-fgm-protocol/bash/cleanup_latex_pres.bash
